b3ccbab276c80e5c7b5b7825b8318c58ce2261fb
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-01-21 18:13:49 +0530

    MODIFIED: Check option

--- snakelader.java
+++ snakelader.java
@@ -1,13 +1,33 @@
 public class snakelader
 {
+	public static final int start_position=0;
+
 	public static void main(String[] args)
 	{
-		int start_Position,player1,dies;
+		int player1=0;
 
-		start_Position=0;
-		player1=0;
-		dies = (int)(Math.random()*6) + 1;
 
-		System.out.println("the dies roll: "+dies);
+		int check_option = (int) Math.floor(Math.random()*10)%3; 
+		System.out.println("the check option is :" +check_option);
+
+		int dies = (int) Math.floor(Math.random()*6+1);
+ 		System.out.println("the value of dies is:" +dies);
+
+		switch(check_option)
+		{
+					case 1:
+					 	System.out.println("LADDER");
+					 	player1=player1+dies;
+						System.out.println("player position is :"+player1);
+						break;
+
+					case 2:
+                				System.out.println("SNAKE");
+                                         	player1=player1-dies;
+                                        	System.out.println("player position is :"+player1);
+                                       		 break;
+					default:
+						System.out.println("no player");
+		}
 	}
 }
