5ddc1ae8a6d6536199338bab5df15a2bf1e17a30
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-01-21 22:45:03 +0530

    ADDED: Playing with two players

--- snakelader.java
+++ snakelader.java
@@ -4,44 +4,85 @@ public class snakelader
 
 	public static void main(String[] args)
 	{
-
-		int player1=0;
-		int count=0;
-		while(player1 != 100)
+		int player=0;
+		int player1=start_position;
+		int player2=start_position;
+		int count1=0;
+		int count2=0;
+		int turn=0;
+		while(player != 100)
 		{
+			if(turn == 0)
+			{
+				player=player1;
+				turn++;
+			}
+			else if(turn == 1)
+			{
+				player=player2;
+				turn--;
+			}
+
 			int check_option = (int) Math.floor(Math.random()*10)%3; 
 			System.out.println("the check option is :" +check_option);
 
 			int dies = (int) Math.floor(Math.random()*6+1);
  			System.out.println("the value of dies is:" +dies);
 
-			count++;
+			if(turn == 1)
+			{
+				count1++;
+			}
+			else if(turn == 0)
+			{
+				count2++;
+			}
 			switch(check_option)
 			{
 					case 1:
 					 	System.out.println("LADDER");
-					 	player1=player1+dies;
-						System.out.println("player position is :"+player1);
+					 	player=player+dies;
+						System.out.println("player position is :"+player);
 						break;
 
 					case 2:
                 				System.out.println("SNAKE");
-						if(player1<=0)
+						if(player<=0)
 						{
-							player1=0;  //player1 starts from 0
+							player=0;  //player1 starts from 0
 						}
-						else if(player1>0)
+						else if(player>0)
 						{
-                                         		player1=player1-dies;
+                                         		player=player-dies;
 						}
-                                        	System.out.println("player position is :"+player1);
+                                        	System.out.println("player position is :"+player);
                                        		 break;
 					default:
 						System.out.println("no player");
 			}
+			if(turn == 1)
+			{
+				player1=player;
+			}
+			else if(turn == 0)
+			{
+				player2=player;
+			}
+			else
+			{
+				System.out.println("you are at player" +player);
+			}
+
 		}
-			System.out.println("!!!!!!!!!!you win!!!!!!!!!!!!");
-			System.out.println("total dies count is :"+count);
+		if(player1 == 100)
+		{
+			System.out.println("!!!!!!!!!!Player 1 win!!!!!!!!!!!!");
+			System.out.println("total dies count is :"+count1);
+		}
+		else if(player2 == 100)
+		{
+			System.out.println("!!!!!!!!!!!!!!player2 win !!!!!!!!!!!!!!!!!!!!!");
+			System.out.println("total dies count is :"+count2);
+		}
 	}
 }
-
