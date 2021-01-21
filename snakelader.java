655dfcce3a203f42d92801177b477240fe0ad500
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-01-21 13:04:50 +0530

    MODIFIED: dies rolled

--- snakelader.java
+++ snakelader.java
@@ -2,9 +2,12 @@ public class snakelader
 {
 	public static void main(String[] args)
 	{
-		int start_Position,player1;
+		int start_Position,player1,dies;
 
 		start_Position=0;
 		player1=0;
+		dies = (int)(Math.random()*6) + 1;
+
+		System.out.println(+dies);
 	}
 }
